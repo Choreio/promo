f44bddd60cea3dd9f7098c85140c32397dd04935
EnderRazer <49989058+EnderRazer@users.noreply.github.com>
2025-01-27 18:19:04 +0300

    Updates

--- index.js
+++ index.js
@@ -9,23 +9,54 @@ function rotateCard(event, card) {
 function resetCard(card) {
     card.style.setProperty('--mouse-x', 0);
     card.style.setProperty('--mouse-y', 0);
-    card.style.transform = 'rotateX(0) rotateY(0)';
 }
 
 const html = document.getElementsByTagName("html")[0];
+const button = document.getElementById("redirect-button");
 const themeToggleBtn = document.getElementById("theme-toggle-btn");
 const themeIcon = document.getElementById("theme-icon");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (prefersDarkScheme.matches) {
+  // User has dark mode set (either OS or browser-level setting)
+  console.log("Dark mode is preferred by the system.");
+  html.setAttribute("data-bs-theme", "dark");
+  themeIcon.classList.remove("fa-moon");
+  themeIcon.classList.add("fa-sun");
+
+  button.classList.remove("btn-dark");
+  button.classList.add("btn-light");
+
+  if(!localStorage.getItem("theme"))
+    localStorage.setItem("theme", "dark");
+} else {
+  // Light mode or no preference
+  console.log("Light mode is preferred by the system.");
+  html.setAttribute("data-bs-theme", "light");
+    themeIcon.classList.remove("fa-sun");
+    themeIcon.classList.add("fa-moon");
+    button.classList.remove("btn-dark");
+    button.classList.add("btn-light");
+    if(!localStorage.getItem("theme"))
+        localStorage.setItem("theme", "dark");
+}
 
 // Optional: Load previously saved theme from localStorage
 const currentTheme = localStorage.getItem("theme") || "light";
 if (currentTheme === "dark") {
+  console.log("Dark mode is loaded from local storage.");
   html.setAttribute("data-bs-theme", "dark");
   themeIcon.classList.remove("fa-moon");
   themeIcon.classList.add("fa-sun");
+  // Save theme preference
+  localStorage.setItem("theme", "dark");
 } else {
+    console.log("Light mode is loaded from local storage.");
     html.setAttribute("data-bs-theme", "light");
     themeIcon.classList.remove("fa-sun");
     themeIcon.classList.add("fa-moon");
+    // Save theme preference
+    localStorage.setItem("theme", "light");
 }
 
 // Listen for toggle button clicks
